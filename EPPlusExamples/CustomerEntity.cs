3a47aea7d67c5313e625cd9adbcf3992b7510b36
MrRajeshRai <rajeshkrist@gmail.com>
2018-01-07 00:35:54 -0800

    Added repo

---
+++ EPPlusExamples/CustomerEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusExamples
+{
+   
+    public static class CustomerEntity
+    {
+        public static List<Customer> GetCustomers()
+        {
+            List<Customer> customerList = new List<Customer>
+            {
+                new Customer {CustomerName = "Customer 1", City = "Mumbai" },
+                new Customer {CustomerName = "Customer 2", City = "Redmond"},
+                new Customer {CustomerName = "Customer 3", City = "Chennai" },
+                new Customer {CustomerName = "Customer 4", City = "Bangalore"},
+                new Customer {CustomerName = "Customer 5", City = "Hyderabad"}
+            };
+
+            return customerList;
+        }
+    }
+
+    public class Customer
+    {
+        [DisplayName("Customer Name")]
+        public string CustomerName { get; set; }
+
+        public string City { get; set; }        
+    }
+}
---
+++ EPPlusExamples/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Configuration;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Linq;
+using OfficeOpenXml.Table;
+using System.Collections.Generic;
+
+namespace EPPlusExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var package = new ExcelPackage())
+            {
+         
+                var worksheet = package.Workbook.Worksheets.Add("Weekly Usage");
+                worksheet.Cells["A1"].LoadFromCollection(CustomerEntity.GetCustomers(), true, TableStyles.Medium6);
+                ApplyStyleToSheet(ref worksheet);
+
+                package.SaveAs(new FileInfo(@"C:\test\excel.xlsx"));
+            }
+        }
+
+        static void ApplyStyleToSheet(ref ExcelWorksheet worksheet)
+        {
+                   
+            //Format the header 
+            using (ExcelRange range = worksheet.Cells["A1:B1"])
+            {
+                range.Style.Font.Bold = true;
+                range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(90, 135, 210));
+                range.Style.Font.Color.SetColor(Color.FromArgb(255, 255, 255));
+            }
+
+            for (int i = 1; i <= worksheet.Dimension.End.Column; i++)
+            {
+                worksheet.Column(i).AutoFit();
+            }
+        }
+    }
+}
